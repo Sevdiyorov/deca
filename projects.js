4592a5be2f9a57fa0c914609f927bee6a81622f8
Muhammadali <sevdiorov@gmail.com>
2026-07-15 15:23:43 +0500

    Add prj +

--- projects.js
+++ projects.js
@@ -2,6 +2,6 @@ const projects = [
   {
     title: "Vocal",
     url: "https://vocal.deca.uz",
-    img: "https://cdn.vectorstock.com/i/500p/22/59/retro-microphone-icon-vector-54222259.jpg"
+    img: "https://github.com/Sevdiyorov/Vocal/blob/main/public/icon-192.png"
   },
 ];
