c9294ea10affabb8280548934960b4a7e68cd900
Muhammadali <sevdiorov@gmail.com>
2026-07-21 13:06:45 +0500

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
-    title: "ITunes X Deca",
-    url: "https://ixd.deca.uz",
-    img: "https://raw.githubusercontent.com/Sevdiyorov/Vocal/main/public/IXD.png"
+    title: "",
+    url: "",
+    img: ""
   },
 ];
