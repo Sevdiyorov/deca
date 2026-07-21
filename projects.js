bb61e23aa40d3413350428c40971459ed12311d0
Muhammadali <sevdiorov@gmail.com>
2026-07-21 13:11:11 +0500

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,6 +2,6 @@ const projects = [
   {
     title: "Google",
     url: "https://google.com",
-    img: "https://share.google/ZCjTCalmI7WDGSnYi"
+    img: "https://share.google/xVsCODtpvzsHV5H0U"
   },
 ];
