79520d21c043f970268aaaefa5f735200492d594
Muhammadali <sevdiorov@gmail.com>
2026-09-15 20:14:37 +0500

    Add files via upload

--- projects.js
+++ projects.js
@@ -5,3 +5,11 @@ const projects = [
     img: "images/bird-logo.png"
   },
 ];
+
+const grid = document.getElementById('projects-grid');
+grid.innerHTML = projects.map(p => `
+    <a href="${p.url}" target="_blank" rel="noreferrer" class="pjt-card">
+        <img src="${p.img}" alt="${p.title}" draggable="false">
+        <span>${p.title}</span>
+    </a>
+`).join('');
